e346842c8ecdc351a6d907fc9b6568e60457bd05
Emmaewan <144682952+Emmaewan@users.noreply.github.com>
2023-09-21 14:11:49 +0200

    Add files via upload

---
+++ Programmering 2/js/script.js
@@ -0,0 +1,11 @@
+function togglePopup() {
+    const popup = document.getElementById("popup")
+
+    popup.classList.toggle("hidden")
+}
+
+function showImage() {
+    const image = document.getElementById("option")
+
+    image.classList.toggle("hidden")
+}
